446f5455df328523c19dbdf0f4875c0873bcef88
AGFH5300 <dxb.avg@gmail.com>
2026-01-14 19:43:18 +0400

    Restore home sections before services

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from "wouter";
 
+import Brands from "./pages/Brands";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Switch>
+      <Route path="/brands" component={Brands} />
       <Route path="/contact" component={Contact} />
       <Route path="/" component={Home} />
     </Switch>
